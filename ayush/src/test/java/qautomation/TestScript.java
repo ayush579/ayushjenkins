d27c106c1868b5eaf786abbcab35fd4d59fef9eb
ayush579 <67556399+ayush579@users.noreply.github.com>
2023-10-08 12:51:33 +0530

    Add files via upload

---
+++ ayush/src/test/java/qautomation/TestScript.java
@@ -0,0 +1,24 @@
+package qautomation;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.asserts.*;
+
+import org.testng.annotations.AfterTest;
+public class TestScript {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
